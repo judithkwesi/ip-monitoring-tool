29bfe70fe9c37d07ae74aa1ee123c750deb0c3fa
Charles Kasasira <charleskasasira01@gmail.com>
2023-07-25 08:18:47 +0300

    add .gitignore

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,20 @@
 #Telling django to render the views .html file
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib import messages
 
 
 # Create your views here. 
 
-@login_required
+@login_required(login_url='login')
 def dashboard(request):   #Main screen
     return render(request, 'registration/dashboard.html', {'section': 'dashboard'})
+
+
+@login_required(login_url='login')
+def logout_user(request):
+	logout(request)
+	messages.success(request, ('Successful logged out!'))
+	return redirect('login')
