f2200416d9ceb25a214a195d73b38266cbb4ad69
Charles Kasasira <charleskasasira01@gmail.com>
2023-07-25 19:47:09 +0300

    update urls

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,4 +1,3 @@
-from unicodedata import name
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
@@ -7,8 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(next_page=None), name='logout'),
-    path('logout/', views.logout_user, name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page=None), name='logout'),  
     
     # password change
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
