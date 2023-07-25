223f5644d03dbfbfdab631f5e260b823b11f072c
Charles Kasasira <charleskasasira01@gmail.com>
2023-07-25 21:39:58 +0300

    forms

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,7 +6,9 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page=None), name='logout'),  
+    # path('logout/', auth_views.LogoutView.as_view(next_page=None), name='logout'),  
+    # path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('logout/', views.logout_user, name='logout'),
     
     # password change
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
