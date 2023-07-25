ae2e6345b9847d468069337fd375ab1f933db227
Charles Kasasira <charleskasasira01@gmail.com>
2023-07-25 21:09:54 +0300

    forms

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -3,5 +3,5 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(required=True, min_length=3) 
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(required=True, min_length=3, widget=forms.TextInput(attrs={'placeholder': 'Enter your username'})) 
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password'}))
