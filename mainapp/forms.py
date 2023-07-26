fa92af6a5a3bb952fdf64b04fa796876d895d999
Charles Kasasira <charleskasasira01@gmail.com>
2023-07-26 08:08:03 +0300

    Charles (#7)
    * Added README.md
    
    * add .gitignore
    
    * new changes
    
    * login css styling
    
    * update urls
    
    * forms.py
    
    * forms.py
    
    * forms
    
    * forms
    
    * update navbar
    
    * style: menu item
    
    * style: menu item
    
    * style: hightlight selected menu
    
    * chore: content structure
    
    * chore: content structure
    
    * add: monitor select box
    
    * refactor: html structure
    
    * refactor: html structure
    
    * refactor: html structure

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,8 +1,7 @@
 #This will have codes for the login form
 
 from django import forms
-from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
-    username = forms.CharField() 
-    password = form.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(required=True, min_length=3, widget=forms.TextInput(attrs={'placeholder': 'Enter your username'})) 
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password'}))
