aa1e337290bc841e89ed4972bb7e3ef096f5236e
Charles Kasasira <charleskasasira01@gmail.com>
2023-07-26 17:45:40 +0300

    Sites Download interval 12hours (#14)
    * Added README.md
    
    * add .gitignore
    
    * new changes
    
    * login css styling
    
    * update urls
    
    * forms.py
    
    * forms.py
    
    * forms
    
    * forms
    
    * update navbar
    
    * style: menu item
    
    * style: menu item
    
    * style: hightlight selected menu
    
    * chore: content structure
    
    * chore: content structure
    
    * add: monitor select box
    
    * refactor: html structure
    
    * refactor: html structure
    
    * refactor: html structure
    
    * gitignore: add .DS_Store files
    
    * change session age to 3600
    
    * add: renu favicon
    
    * delete .DS_Store
    
    * deleted sqlitte
    
    * chore: remove unsed packages
    
    * feat: download site files
    
    * Debug: false
    
    * add scheduler package
    
    * change to hours for sites download

--- mainapp/apps.py
+++ mainapp/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class MainappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mainapp'
+
+    def ready(self):
+        from .scheduler import updater
+        updater()
