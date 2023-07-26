2a96c0ebc9ff8cd5b285b09997ef66db78f44f61
Charles Kasasira <charleskasasira01@gmail.com>
2023-07-26 08:08:03 +0300

    Charles (#7)
    * Added README.md
    
    * add .gitignore
    
    * new changes
    
    * login css styling
    
    * update urls
    
    * forms.py
    
    * forms.py
    
    * forms
    
    * forms
    
    * update navbar
    
    * style: menu item
    
    * style: menu item
    
    * style: hightlight selected menu
    
    * chore: content structure
    
    * chore: content structure
    
    * add: monitor select box
    
    * refactor: html structure
    
    * refactor: html structure
    
    * refactor: html structure

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,10 @@
 #Telling django to render the views .html file
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib import messages
+from django.contrib.auth.views import LogoutView
 
 
 # Create your views here. 
@@ -12,9 +13,21 @@ from django.contrib import messages
 def dashboard(request):   #Main screen
     return render(request, 'registration/dashboard.html', {'section': 'dashboard'})
 
+@login_required(login_url='login')
+def users(request):
+    return render(request, 'registration/users.html', {'section': 'users'})
+
+@login_required(login_url='login')
+def settings(request):
+    return render(request, 'registration/settings.html', {'section': 'settings'})
 
 @login_required(login_url='login')
 def logout_user(request):
 	logout(request)
 	messages.success(request, ('Successful logged out!'))
+        
+	response = HttpResponse()
+	response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+	response['Pragma'] = 'no-cache'
+	response['Expires'] = '0'
 	return redirect('login')
