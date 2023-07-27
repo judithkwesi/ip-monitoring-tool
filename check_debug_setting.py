21b1784f1cec8a0269303ffafab14bd089457f51
Charles Kasasira <charleskasasira01@gmail.com>
2023-07-27 12:54:51 +0300

    Testing Actions when debug is left true (#32)
    * Added README.md
    
    * add .gitignore
    
    * new changes
    
    * login css styling
    
    * update urls
    
    * forms.py
    
    * forms.py
    
    * forms
    
    * forms
    
    * update navbar
    
    * style: menu item
    
    * style: menu item
    
    * style: hightlight selected menu
    
    * chore: content structure
    
    * chore: content structure
    
    * add: monitor select box
    
    * refactor: html structure
    
    * refactor: html structure
    
    * refactor: html structure
    
    * gitignore: add .DS_Store files
    
    * change session age to 3600
    
    * add: renu favicon
    
    * delete .DS_Store
    
    * deleted sqlitte
    
    * chore: remove unsed packages
    
    * feat: download site files
    
    * Debug: false
    
    * add scheduler package
    
    * change to hours for sites download
    
    * gitignore: Exception
    
    * Hide secret key
    
    * fix conflict
    
    * set ALLOWED HOSTS
    
    * Github Actions
    
    * update github actions
    
    * test
    
    * test
    
    * changed check debug settngs
    
    * Test Actions
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * updated settings
    
    * updated settings
    
    * Testing Actions when debug is left true
    
    * Testing Actions when debug is left true
    
    * updated settings
    
    * Testing Actions when debug is left true
    
    * check Debug in Settings
    
    * Testing Actions when debug is left true
    
    * Actions environment variables
    
    * Testing Actions when debug is left true
    
    * Actions environment variables
    
    * Testing Actions when debug is left true
    
    * Actions environment variables
    
    * Testing Actions when debug is left true
    
    * Testing Actions when debug is left true
    
    * Testing Actions when debug is left true
    
    * Testing Actions when debug is left true
    
    * Testing Actions when debug is left true

--- check_debug_setting.py
+++ check_debug_setting.py
@@ -1,6 +1,7 @@
 import os
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+import sys
 
 # Set the Django settings module to your_project_name.settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.settings")
@@ -11,7 +12,7 @@ try:
     
     # Check the DEBUG setting
     if settings.DEBUG:
-        raise ValueError("ERROR: Debug mode must be set to False for the production environment.")
+        sys.exit("ERROR: Debug mode must be set to False for the production environment.")
 except ImproperlyConfigured as e:
     # If settings are not configured properly, the script will raise an ImproperlyConfigured exception.
     # Handle the exception as needed, e.g., exit with an error or log the issue.
