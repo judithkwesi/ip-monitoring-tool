0e9edc6589547dc72df4a3d9bedba90ae0cda7db
SsenabulyaDavid <138119782+SsenabuyaDavid@users.noreply.github.com>
2023-07-27 15:01:07 +0300

    David (#34)
    * code modifications
    
    * code modifications

--- mainapp/apps.py
+++ mainapp/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class MainappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mainapp'
-
-    def ready(self):
-        from .scheduler import updater
-        updater()
