5613c1ea1e317d2c31840ca2356b59b7d48f88da
Charles Kasasira <charleskasasira01@gmail.com>
2023-07-27 11:52:08 +0300

    Charles (#27)
    * Added README.md
    
    * add .gitignore
    
    * new changes
    
    * login css styling
    
    * update urls
    
    * forms.py
    
    * forms.py
    
    * forms
    
    * forms
    
    * update navbar
    
    * style: menu item
    
    * style: menu item
    
    * style: hightlight selected menu
    
    * chore: content structure
    
    * chore: content structure
    
    * add: monitor select box
    
    * refactor: html structure
    
    * refactor: html structure
    
    * refactor: html structure
    
    * gitignore: add .DS_Store files
    
    * change session age to 3600
    
    * add: renu favicon
    
    * delete .DS_Store
    
    * deleted sqlitte
    
    * chore: remove unsed packages
    
    * feat: download site files
    
    * Debug: false
    
    * add scheduler package
    
    * change to hours for sites download
    
    * gitignore: Exception
    
    * Hide secret key
    
    * fix conflict
    
    * set ALLOWED HOSTS
    
    * Github Actions
    
    * update github actions
    
    * test
    
    * test
    
    * changed check debug settngs
    
    * Test Actions
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * updated settings
    
    * updated settings
    
    * Testing Actions when debug is left true
    
    * Testing Actions when debug is left true
    
    * updated settings
    
    * Testing Actions when debug is left true
    
    * check Debug in Settings

--- check_debug_setting.py
+++ check_debug_setting.py
@@ -3,16 +3,21 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
 # Set the Django settings module to your_project_name.settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.settings")
 
 try:
-    # Try to configure Django settings if not already configured
-    settings.configure()
+    # Import the Django settings module
+    from django.conf import settings
+    
     # Check the DEBUG setting
     if settings.DEBUG:
         raise ValueError("ERROR: Debug mode must be set to False for the production environment.")
 except ImproperlyConfigured as e:
-    # If settings are already configured, the script is being run within the Django context.
-    # You can handle it as needed, e.g., exit with an error or log the issue.
+    # If settings are not configured properly, the script will raise an ImproperlyConfigured exception.
+    # Handle the exception as needed, e.g., exit with an error or log the issue.
+    # For this example, let's print the error message.
+    print(f"ERROR: {e}")
+except Exception as e:
+    # Handle any other exceptions that may occur during the import or check process.
     # For this example, let's print the error message.
     print(f"ERROR: {e}")
