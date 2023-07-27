27fecdec02dab5bbd0d629f41fc513e990fe3b22
Charles Kasasira <charleskasasira01@gmail.com>
2023-07-27 11:44:23 +0300

    Testing Actions when debug is left true

--- check_debug_setting.py
+++ check_debug_setting.py
@@ -1,9 +1,18 @@
-# check_debug_setting.py
-
+import os
 from django.conf import settings
-import sys
+from django.core.exceptions import ImproperlyConfigured
 
-settings.configure()
+# Set the Django settings module to your_project_name.settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")
 
-if settings.DEBUG:
-    sys.exit("ERROR: Debug mode must be set to False for the production environment.")
+try:
+    # Try to configure Django settings if not already configured
+    settings.configure()
+    # Check the DEBUG setting
+    if settings.DEBUG:
+        raise ValueError("ERROR: Debug mode must be set to False for the production environment.")
+except ImproperlyConfigured as e:
+    # If settings are already configured, the script is being run within the Django context.
+    # You can handle it as needed, e.g., exit with an error or log the issue.
+    # For this example, let's print the error message.
+    print(f"ERROR: {e}")
