c746179e31f26094abb174268b32fa6f6b621f4d
Charles Kasasira <charleskasasira01@gmail.com>
2023-07-27 11:17:15 +0300

    Test Actions

--- .github/check_debug_setting.py
+++ .github/check_debug_setting.py
@@ -2,6 +2,13 @@
 
 from django.conf import settings
 import sys
+import os
+
+# Set the Django settings module to your_project_name.settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")
+
+# Configure Django settings
+settings.configure()
 
 if settings.DEBUG:
     sys.exit("ERROR: Debug mode must be set to False for the production environment.")
