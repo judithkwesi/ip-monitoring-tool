96222fd3e7c0712b3eb25793216e370c118dc402
Charles Kasasira <charleskasasira01@gmail.com>
2023-07-27 20:08:40 +0300

    feature rate limiting

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('users/', views.users, name='users'),
     path('settings/', views.settings, name='settings'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', views.login_view, name='login'),
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(next_page=None), name='logout'), 
     path('logout/', views.logout_user, name='logout'),
     
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,40 @@
-#Telling django to render the views .html file
-
 from django.shortcuts import render, redirect, HttpResponse
+from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
 from django.contrib import messages
-import subprocess
 from django.contrib.auth.models import User
-
+from django.core.cache import cache
+# from django_ratelimit.decorators import ratelimit
 
 # Create your views here. 
+# @ratelimit(key='ip', rate='5/h', block=True)
+def login_view(request):
+      if request.method == 'POST':
+        user_ip = request.META.get('REMOTE_ADDR')
+        key = f'login_attempts:{user_ip}'
+        attempts = cache.get(key, 0)
+
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            cache.delete(key)
+            login(request, user)
+            return redirect('dashboard')
+        else:
+             if attempts >= 5:  # Limiting to 5 login attempts per hour
+                  print("Too many login attempts. Try again later.")
+                  return HttpResponseForbidden("<h1>403 Forbidden.</h1><p>Too many failed login attempts. Try again later.</p>")
+             else:
+                  if(username):
+                       cache.set(key, attempts + 1, 3600)
+                       return redirect('login')
+            
+      else:
+        return render(request, 'registration/login.html', {})
 
 @login_required(login_url='login')
 def dashboard(request):   #Main screen
