278adbc468b9834c38b3025ca65a7d3f03948388
SsenabulyaDavid <138119782+SsenabuyaDavid@users.noreply.github.com>
2023-07-27 15:01:07 +0300

    David (#34)
    * code modifications
    
    * code modifications

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+<<<<<<< HEAD
+    path('admin/', admin.site.urls), #Specifies path to admin webpage
+    path('', include('mainapp.urls')), #Specifies path to login webpage
+=======
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls')),
+>>>>>>> 9b9425f00591eb859dc79da1809734c02945206a
 ]
