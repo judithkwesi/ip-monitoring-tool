04d76d90cbae317aa1f5df3be58e840b40508996
Charles Kasasira <charleskasasira01@gmail.com>
2023-07-28 02:58:02 +0300

    Charles (#38)
    * Added README.md
    
    * add .gitignore
    
    * new changes
    
    * login css styling
    
    * update urls
    
    * forms.py
    
    * forms.py
    
    * forms
    
    * forms
    
    * update navbar
    
    * style: menu item
    
    * style: menu item
    
    * style: hightlight selected menu
    
    * chore: content structure
    
    * chore: content structure
    
    * add: monitor select box
    
    * refactor: html structure
    
    * refactor: html structure
    
    * refactor: html structure
    
    * gitignore: add .DS_Store files
    
    * change session age to 3600
    
    * add: renu favicon
    
    * delete .DS_Store
    
    * deleted sqlitte
    
    * chore: remove unsed packages
    
    * feat: download site files
    
    * Debug: false
    
    * add scheduler package
    
    * change to hours for sites download
    
    * gitignore: Exception
    
    * Hide secret key
    
    * fix conflict
    
    * set ALLOWED HOSTS
    
    * Github Actions
    
    * update github actions
    
    * test
    
    * test
    
    * changed check debug settngs
    
    * Test Actions
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * updated settings
    
    * updated settings
    
    * Testing Actions when debug is left true
    
    * Testing Actions when debug is left true
    
    * updated settings
    
    * Testing Actions when debug is left true
    
    * check Debug in Settings
    
    * Testing Actions when debug is left true
    
    * Actions environment variables
    
    * Testing Actions when debug is left true
    
    * Actions environment variables
    
    * Testing Actions when debug is left true
    
    * Actions environment variables
    
    * Testing Actions when debug is left true
    
    * Testing Actions when debug is left true
    
    * Testing Actions when debug is left true
    
    * Testing Actions when debug is left true
    
    * Testing Actions when debug is left true
    
    * Testing Actions when debug is left true
    
    * Testing Protected branch
    
    * users
    
    * Users list
    
    * fix conflict
    
    * check for renu ips
    
    * feature rate limiting
    
    * add __pycache__ to .gitignore
    
    * remove messages
    
    * Refactor: Code modification
    
    * update .gitignore: removed pycache from vc
    
    * update .gitignore: removed myapp/__pycache from vc
    
    * update .gitignore: removed myapp/__pycache from vc

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,23 +1,65 @@
-#Telling django to render the views .html file
-
 from django.shortcuts import render, redirect, HttpResponse
+from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
-from django.contrib import messages
-import subprocess
+from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.models import User
-
+from django.core.cache import cache
+from .forms import MySelectForm
+# from django_ratelimit.decorators import ratelimit
 
 # Create your views here. 
+# @ratelimit(key='ip', rate='5/h', block=True)
+def login_view(request):
+      if request.method == 'POST':
+        user_ip = request.META.get('REMOTE_ADDR')
+        key = f'login_attempts:{user_ip}'
+        attempts = cache.get(key, 0)
+
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            cache.delete(key)
+            login(request, user)
+            return redirect('dashboard')
+        else:
+             if attempts >= 5:  # Limiting to 5 login attempts per hour
+                  print("Too many login attempts. Try again later.")
+                  return HttpResponseForbidden("<h1>403 Forbidden.</h1><p>Too many failed login attempts. Try again later.</p>")
+             else:
+                  if(username):
+                       cache.set(key, attempts + 1, 3600)
+                       return redirect('login')
+            
+      else:
+        return render(request, 'registration/login.html', {})
 
 @login_required(login_url='login')
 def dashboard(request):   #Main screen
-    return render(request, 'registration/dashboard.html', {'section': 'dashboard'})
+    if request.method == 'POST':
+        form = MySelectForm(request.POST)
+        if form.is_valid():
+            selected_option = form.cleaned_data['select_choice']
+
+            selected_option_label = dict(form.fields['select_choice'].choices).get(selected_option)
+            # Render the same template with updated options
+            return render(request, 'registration/dashboard.html', {'section': 'dashboard', 'form': form, 'selected_option': selected_option, 'selected_label': selected_option_label})
+    else:
+        form = MySelectForm(initial={'select_choice': 'option1'})
+        selected_option = form.initial['select_choice']
+
+        selected_option_label = dict(form.fields['select_choice'].choices).get(selected_option)
+
+        return render(request, 'registration/dashboard.html', {'section': 'dashboard', 'form': form, 'selected_option': selected_option, 'selected_label': selected_option_label})
+
+    return render(request, 'registration/dashboard.html', {'section': 'dashboard', 'form': ""})
 
 @login_required(login_url='login')
 def users(request):
     users = User.objects.all()
-    return render(request, 'registration/users.html', {'users': users})
+    return render(request, 'registration/users.html', {'users': users, 'section': 'users'})
 
 @login_required(login_url='login')
 def settings(request):
@@ -26,7 +68,6 @@ def settings(request):
 @login_required(login_url='login')
 def logout_user(request):
 	logout(request)
-	messages.success(request, ('Successful logged out!'))
         
 	response = HttpResponse()
 	response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
@@ -38,7 +79,6 @@ def logout_user(request):
 @login_required(login_url='login')
 def logout_user(request):
 	logout(request)
-	messages.success(request, ('Successful logged out!'))
         
 	response = HttpResponse()
 	response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
