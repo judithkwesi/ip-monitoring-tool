eeaef25b6a5b447790ab0b04e54f3b2c122eb014
Charles Kasasira <charleskasasira01@gmail.com>
2023-07-27 09:32:23 +0300

    Generate & Hide secret_keys (#17)
    * Added README.md
    
    * add .gitignore
    
    * new changes
    
    * login css styling
    
    * update urls
    
    * forms.py
    
    * forms.py
    
    * forms
    
    * forms
    
    * update navbar
    
    * style: menu item
    
    * style: menu item
    
    * style: hightlight selected menu
    
    * chore: content structure
    
    * chore: content structure
    
    * add: monitor select box
    
    * refactor: html structure
    
    * refactor: html structure
    
    * refactor: html structure
    
    * gitignore: add .DS_Store files
    
    * change session age to 3600
    
    * add: renu favicon
    
    * delete .DS_Store
    
    * deleted sqlitte
    
    * chore: remove unsed packages
    
    * feat: download site files
    
    * Debug: false
    
    * add scheduler package
    
    * change to hours for sites download
    
    * gitignore: Exception
    
    * Hide secret key

--- myapp/settings.py
+++ myapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7j)8rkkk&b7ge$k2)tg#4r3scck5bj^!k-56r-qdot2a=*(si^'
+with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
+	SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
