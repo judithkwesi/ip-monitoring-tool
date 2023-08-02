54c7c39e79c1e466b761f22e4e97d925bb87c749
SsenabulyaDavid <138119782+SsenabuyaDavid@users.noreply.github.com>
2023-08-02 12:26:29 +0300

    Feature/IPv6 (#47)
    * Python program that checks whether the first fixed 32 bits of a given IPv6 address are the same as those of RENU's IPv6 address
    
    * Added functionality to the code to enable it check whether the prefix length is greater than 32.
    
    * Program checks whether input by the user is a subnet of the IPv6 address  2c0f:f6d0::/32
    
    * Program that can accept a text file and then identifies whether the ipv6 addresses in the file are subnets of 2c0f:f6d0
    
    * Python program that include a function that can be called to print out Ipv6 addresses of a text file which are similar to a reference ipv6 address
    
    * Python program that include a function that can be called to print out Ipv6 addresses of a text file which are similar to a reference ipv6 address

--- mainapp/Check_for_IPv6_renu_ip.py
+++ mainapp/Check_for_IPv6_renu_ip.py
@@ -1,64 +1,84 @@
 import ipaddress
 
-input_file = "./sites/spamhausv6.txt"
+# function to expand IPv6 address into binary(bits)
+def expand_ipv6_to_bits(ipv6_address):
+    try:
+        # Convert IPv6 address to an IPv6Address object
+        ipv6_addr = ipaddress.IPv6Address(ipv6_address)
 
-with open(input_file, 'r') as file:
-    lines = file.readlines()
+        # Get the full binary representation of the IPv6 address
+        ipv6_binary = "".join(format(int(x, 16), '04b') for x in ipv6_addr.exploded.replace(':', ''))
 
-for line in lines:
-    line = line.strip()
-    if line and not line.startswith(';'):
-        ipv6_full_address = line.split(';')[0].strip()
+        return ipv6_binary
+    except ipaddress.AddressValueError:
+        return None
+    
 
-        def check_ipv6_subnet_length(ipv6_address):
-            try:
-                # Split the IPv6 address and prefix length
-                address, prefix_length = ipv6_address.split('/')
+def check_ipv6_subnet_length(ipv6_address):
+    try:
+        # Split the IPv6 address and prefix length
+        _, prefix_length = ipv6_address.split('/')
 
-                # Convert the IPv6 address to an IPv6Address object
-                ipv6_addr = ipaddress.IPv6Address(address)
+        # Convert the prefix length to an integer
+        prefix_length = int(prefix_length)
 
-                # Convert the prefix length to an integer
-                prefix_length = int(prefix_length)
-
-                # Check if the prefix length is less than to 32
-                return prefix_length < 32
-                
-            except ValueError:
-                return False
-
-        def check_ipv6_prefix(ipv6_address):
-            try:
-                # Split the IPv6 address and prefix length
-                address, prefix_length = ipv6_address.split('/')
-
-                # Convert IPv6 address to an IPv6Address object
-                ipv6_addr = ipaddress.IPv6Address(address)
-
-                # Convert subnet address to an IPv6Address object
-                subnet_address = ipaddress.IPv6Address("2c0f:f6d0::")
-
-                # Get the first 32 bits (first 4 blocks) of the IPv6 address and the subnet address
-                ipv6_prefix = ipv6_addr.exploded[:19]  # 19 characters represent the first 32 bits (4 blocks)
-                subnet_prefix = subnet_address.exploded[:19]
-
-                # Check if the first 32 bits are similar to "2c0f:f6d0"
-                return ipv6_prefix == subnet_prefix
-            except (ValueError, ipaddress.AddressValueError):
-                return False
-
-        if __name__ == "__main__":
+        # Check if the prefix length is less than to 32
+        return prefix_length < 32
+        
+    except ValueError:
+        return False
             
-            if check_ipv6_subnet_length(ipv6_full_address):
+
+def check_ipv6_prefix(test_ip_address, reference_ip_address):
+    try:
+        # Split the IPv6 address and prefix length
+        address1, _ = test_ip_address.split('/')
+        address2, prefix_length2 = reference_ip_address.split('/')
+
+        # Convert IPv6 addresses to IPv6Address objects
+        ipv6_addr1 = ipaddress.IPv6Address(address1)
+        ipv6_addr2 = ipaddress.IPv6Address(address2)
+
+        # Expands IPv6 addresses into full form
+        ipv6_prefix1 = ipv6_addr1.exploded[:]  
+        ipv6_prefix2 = ipv6_addr2.exploded[:]
+
+        #Expands ipv6 address into bits
+        ipv6_prefix1_in_bits1 = expand_ipv6_to_bits(ipv6_prefix1)
+        ipv6_prefix1_in_bits2 = expand_ipv6_to_bits(ipv6_prefix2)
+
+        # Check if the first bits equal in quantity to the prefix_length2 of both addresses are similar
+        return ipv6_prefix1_in_bits1[:int(prefix_length2)] == ipv6_prefix1_in_bits2[:int(prefix_length2)]
+
+    except (ValueError, ipaddress.AddressValueError):
+        return False
+    
+
+    
+def identify_ipv6_addresses(input_file, reference_ipv6_address):
+
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
+
+    for line in lines:
+        line = line.strip()
+        if line and not line.startswith(';'):
+            ipv6_full_address = line.split(';')[0].strip()
+        
+            if __name__ == "__main__":
                 
-                pass
-            else:
-                
-                if check_ipv6_prefix(ipv6_full_address):
-                    print(f"The ip address {ipv6_full_address} is a subnet.")
+                if check_ipv6_subnet_length(ipv6_full_address):
                     
-                else:
                     pass
+                else:
+                    
+                    if check_ipv6_prefix(ipv6_full_address, reference_ipv6_address):
+                        print(f"The ip address {ipv6_full_address} is a subnet.")
+                        
+                    else:
+                        pass
                    
 
-
+# Calling the identify_ipv6_addresses
+Renu_ip_address = "2c0f:f6d0::/32"
+identify_ipv6_addresses("./sites/spamhausv6.txt", Renu_ip_address)
