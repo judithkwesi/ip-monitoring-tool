7a1067a7043a5bfa82383bc49ec4fade20830720
CharlesKasasira <charleskasasira@gmail.com>
2023-08-18 18:02:29 +0300

    updated deployment script

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -2,9 +2,9 @@
 
 # Constants
 REPO_URL="https://github.com/judithkwesi/ip-monitoring-tool.git"
-PROJECT_DIR="/home/charles/ip-reputation/staging/ip-monitoring-tool"
-VENV_DIR="/home/charles/ip-reputation/staging/dev_env"
-# SYSTEMD_SERVICE="your_service_name.service"
+PROJECT_DIR="/home/ssenabulyadavid/ip-monitoring-tool/ip-monitoring-tool"
+VENV_DIR="/home/ssenabulyadavid/ip-monitoring-tool/env"
+SYSTEMD_SERVICE="ip-monitoring-tool.service"
 
 # Function to update the code from GitHub
 update_code() {
@@ -27,6 +27,7 @@ run_migrations() {
     cd "$PROJECT_DIR"
     source $VENV_DIR/bin/activate
     echo "Activating Virtual Environment..."
+    python manage.py makemigrations mainapp
     python manage.py migrate
 }
 
@@ -36,38 +37,13 @@ run_collectstatic() {
     source $VENV_DIR/bin/activate
     echo "Activating Virtual Environment..."
     chmod +w "$PROJECT_DIR"/static
-
-
     python manage.py collectstatic --noinput
 }
 
 # Function to restart the server
-# restart_server() {
-#     echo "Restarting the server..."
-#     sudo systemctl restart $SYSTEMD_SERVICE
-# }
-
-# Function to stop the program running on port 8001
-stop_program_on_port() {
-    echo "Stopping program on port 8001..."
-    local pids=$(sudo lsof -t -i :8001)
-
-    if [ -z "$pids" ]; then
-        echo "No program found running on port 8001."
-    else
-        for pid in $pids; do
-            sudo kill -9 "$pid"
-            echo "Terminated process with PID: $pid"
-        done
-        echo "Program(s) stopped successfully."
-    fi
-
-    # restart gunicorn
-    # gunicorn ip-monitoring-tool.wsgi:application --bind 127.0.0.1:8001 &
-    nohup gunicorn ip-monitoring-tool.wsgi:application --bind 127.0.0.1:8001 >/dev/null 2>&1 &
-    disown
-
-    echo "Gunicorn restarted on port 8001."
+restart_server() {
+    echo "Restarting the server..."
+    sudo systemctl restart $SYSTEMD_SERVICE
 }
 
 # Main deployment process
@@ -75,9 +51,8 @@ main() {
     update_code
     # install_dependencies
     run_migrations
-    # restart_server
     run_collectstatic
-    stop_program_on_port
+    restart_server
     echo "Deployment successful!"
 }
 
--- .github/workflows/test.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-
-echo "From the bottom, now we are here 1"
-echo "From the bottom, now we are here 2"
-echo "From the bottom, now we are here 3"
